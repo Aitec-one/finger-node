11ff9fc415a538f54e0ad049de7b27dbedbe7003
Islam <ikashkynbek@gmail.com>
2020-05-27 12:37:50 +0600

    finger_test: load from / store to templates.txt

--- finger_node_test/finger_test.js
+++ finger_node_test/finger_test.js
@@ -1,21 +1,42 @@
 const finger = require('./finger');
+const fs = require('fs');
 
-const template = "SjJTUzIxAAADcXIECAUHCc7QAAAvcHYBAAAAg5wgrHHZAOIMiwAdAON/1gD0AGQHWAD4cWELugBuAKML93HyAFoHBQFVAGh+1ABdAO4L3gFkcQEMYwApASgB1XEKAPAPsgAcAON9hgCTAOECPQC0cV4IegCLAIsCeXHrAFoKlgDAAeJwaQDoAOMMqgAJcGcFYgAfAasFTXE4AfQDCQHJAPd+lQCWAF4JKACUce8M+wCfADYKAnDQAFUNsQDOAWRweQDzANkK1gHLcUYLbAAWAScF/XFHAG4PFgHpAHZ/rQUiBfKGxIr5eaMBHQ+G/3KEe/I8+6qKBni+CoD2pPv2BQMVqHmKD2cO/oh/gAIDJebWlD+PUIOgDfqKkv27h6d/6QEhc/uCqIc1lEJ/6h9UDzoYSWyA8yCacYKteQV+vYOqDjcJoYm1DRN9Z/Zw+umOUIMxj+p7cAtqiGsUe4EMdNaLrH+1E8b6qo2IhsaRJQSgidbnGBHd+379cAum+gcHcIJB+Tx2qoTT6OdUaH/Fd/KE8/k2c3+Ae4FpeEp38uW7uuU2AnDOFn8EAP/FdF98AA8AbUX/BlJWegGPT+trSjpLBnHAAWJMCAARAmqwVFsDASoJssALcdEQa1zAYdcBKmJ7amptVv4GaBNwKB56wG9mBVhxfgAVKnqFwpz/w7HAwMEPARX1cPwbwFn/wG0QxCszAUbBwcHAWwdmBHApPndvSgTF+0sBdAQABUk9/RQCWEh6dcD/cQRlwI5EwAMAYlCowhdwK1N9wsBwrMBpKsHAwBMBKpl9wbFxeMB7wUwFDgJnaXeFwmpqjwUDyWxrwmUHAHxyYSlnEAEqdIYHkMGOw2Ro/xEB7HyAs4HC/8JlV5AFAliHhsKVAwHPjv6PBABfkmkrzAEH5WjBYsBUBsWSmRHA/0kHAOleZsKwWwQBKZ6XdwUCfasG+SINADG3YRTBOP/ENxDEK7TGxsaXwWQ+iwoDi7hcwExBwsABF7hBLQcBEMKMQj9yAf3TWsAExYfYFzYHAKjbazrE/UsTARzfw8IHx86y/v9Pwf/8AD8AYe4IYsEFABH3YbFCBQDi92KlBAP8+W08BQCaO14ochFLF2vCA9VeHQXBAxBhI2s6AxOpLlz8BBCYjQD8sgUQj1MGwzgDE0VQOv4DEHeSBvw=";
+const tmplFile = "templates.txt";
 
-finger.verify(template, (err, result) => {
-    console.log(`err: ${err}, result: ${result}`);
-    if (result === "verified") {
-        console.log("verified");
-    } else {
-        console.log("not verified");
-    }
-});
-
-finger.enroll((err, result) => {
+fs.readFile(tmplFile, "utf8", function(err, data) {
     if (err) {
-        console.log("Error: " + err)
+        console.log("error: " + err);
+        return
+    }
+
+    if (data === "") {
+        console.log("Enroll finger");
+        finger.enroll((err, result) => {
+            if (err) {
+                console.log("Error: " + err)
+            } else {
+                console.log("enrolled:");
+                console.log(result);
+                fs.writeFile(tmplFile, result, function(err) {
+                    if(err) {
+                        return console.log(err);
+                    }
+                    console.log("Template was stored");
+                });
+            }
+        });
+
     } else {
-        console.log("enrolled:");
-        console.log(result)
+        console.log("verifing finger");
+        console.log(data);
+        finger.verify(data, (err, result) => {
+            console.log(`err: ${err}, result: ${result}`);
+            if (result === "verified") {
+                console.log("verified");
+            } else {
+                console.log("not verified");
+            }
+        });
     }
 });
+
